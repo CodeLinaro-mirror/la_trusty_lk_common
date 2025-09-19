afea52b6367406a7dee0a95e5a497fb817c87dd8
Weston Carvalho <westoncarvalho@google.com>
2025-09-19 11:19:45 -0500

    rust_support: Provide conversion methods for Uuid
    Make it more ergonomic to construct a `Uuid` from a `uuid_t` and to get
    the `uuid_t` out of a `Uuid`.
    
    Bug: 446003523
    Test: build
    Signed-off-by: Weston Carvalho <westoncarvalho@google.com>
    Change-Id: I4af4c3bcac8823acdacca0642b5935e60cbdd8fb

--- lib/rust_support/uuid.rs
+++ lib/rust_support/uuid.rs
@@ -36,16 +36,26 @@ impl Uuid {
     ) -> Self {
         Self(uuid_t { time_low, time_mid, time_hi_and_version, clock_seq_and_node })
     }
+
+    pub fn c_repr(self) -> uuid_t {
+        self.0
+    }
 }
 
 impl uuid_t {
-    pub fn kernel() -> Self {
+    pub fn kernel() -> &'static Self {
         // SAFETY: This is a bindgen-generated static which is known to be constant
-        unsafe { crate::sys::kernel_uuid }
+        unsafe { &crate::sys::kernel_uuid }
     }
 
-    pub fn zero() -> Self {
+    pub fn zero() -> &'static Self {
         // SAFETY: This is a bindgen-generated static which is known to be constant
-        unsafe { crate::sys::zero_uuid }
+        unsafe { &crate::sys::zero_uuid }
+    }
+}
+
+impl From<uuid_t> for Uuid {
+    fn from(uuid: uuid_t) -> Self {
+        Self(uuid)
     }
 }
