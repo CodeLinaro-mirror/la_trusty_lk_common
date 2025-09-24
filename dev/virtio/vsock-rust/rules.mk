5b4ea798e85608a1298ad4ebd7bccc34ad4ae595
Ayrton Munoz <ayrton@google.com>
2025-09-24 16:36:50 +0000

    dev: virtio: vsock-rust: Add rules.mk option to generate virtio-msg bindings
    When the headers from [1] are mainlined the bindings may be
    generated as part of the build. To eventually support that this
    commit adds the bindgen options used to generate new_bindings.rs
    to rules.mk under a new, disabled VSOCK_WITH_VIRTIO_MSG_HEADERS
    make flag.
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/vireshk/linux.git/log/?h=virtio/msg
    
    Bug: 379677575
    Change-Id: Id4d921f62f871ffb167a7d5c1f672ca8ff1a5625
    Signed-off-by: Ayrton Munoz <ayrton@google.com>

--- dev/virtio/vsock-rust/rules.mk
+++ dev/virtio/vsock-rust/rules.mk
@@ -129,4 +129,58 @@ endif
 
 MODULE_RUST_USE_CLIPPY := true
 
+# TODO: These are the options used to generate new_bindings.rs from the latest version of the
+# virtio-msg headers. Once the headers are in mainline linux and we find a suitable location for
+# them they may be generated as part of the build.
+VSOCK_WITH_VIRTIO_MSG_HEADERS ?= false
+
+ifeq (true,$(call TOBOOL,$(VSOCK_WITH_VIRTIO_MSG_HEADERS)))
+MODULE_BINDGEN_SRC_HEADER := $(LOCAL_DIR)/bindings.h
+
+MODULE_BINDGEN_ALLOW_FILES := \
+	.*virtio_config.h \
+	.*virtio_msg.h \
+	.*virtio_msg_ffa.h \
+
+VIRTIO_MSG_BINDGEN_TYPES := \
+	bus_area_share \
+	bus_area_share_resp \
+	bus_area_unshare \
+	bus_area_unshare_resp \
+	bus_area_release \
+	bus_event_device \
+	bus_ffa_version \
+	bus_ffa_version_resp \
+	bus_get_devices \
+	bus_ping \
+	bus_ping_resp \
+	bus_status \
+	event_avail \
+	event_used \
+	get_config \
+	get_device_info_resp \
+	get_device_status_resp \
+	get_device_status \
+	get_features \
+	get_shm \
+	get_shm_resp \
+	get_vqueue \
+	get_vqueue_resp \
+	reset_vqueue \
+	set_device_status \
+	set_device_status_resp \
+	set_vqueue \
+
+ZEROCOPY_TRAITS := \
+	zerocopy::Immutable \
+	zerocopy::FromBytes \
+	zerocopy::IntoBytes \
+	zerocopy::KnownLayout \
+
+MODULE_BINDGEN_FLAGS := \
+	--with-derive-custom .*=Default \
+    $(foreach type,$(VIRTIO_MSG_BINDGEN_TYPES),$(foreach trait,$(ZEROCOPY_TRAITS),--with-derive-custom $(type)=$(trait)))
+
+endif
+
 include make/library.mk
