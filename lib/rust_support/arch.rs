e12c234bb2d5bee13f91cb71891d199bef6ac5aa
Andrei Homescu <ahomescu@xwf.google.com>
2025-12-20 00:43:11 +0000

    lib: rust_support: Expose cache operations
    Bug: 383197438
    Change-Id: Ie5e6c327f6a71077e90ae66f9075bc506acca0cc
    Signed-off-by: Andrei Homescu <ahomescu@xwf.google.com>

--- lib/rust_support/arch.rs
+++ lib/rust_support/arch.rs
@@ -21,27 +21,26 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#pragma once
+pub fn clean_cache_range<T>(slice: &[T]) {
+    let range = slice.as_ptr_range();
+    // SAFETY: The caller passes in a valid slice and the
+    // only side effects of the kernel function are on the cache.
+    unsafe {
+        crate::sys::arch_clean_cache_range(
+            range.start.addr(),
+            range.end.byte_offset_from_unsigned(range.start),
+        );
+    }
+}
 
-#include <arch/mmu.h>
-#include <arch/ops.h>
-#include <kernel/event.h>
-#include <kernel/mutex.h>
-#include <kernel/thread.h>
-#include <kernel/vm.h>
-#include <lib/ktipc/ktipc.h>
-#include <lib/trusty/handle.h>
-#include <lib/trusty/handle_set.h>
-#include <lib/trusty/ipc.h>
-#include <lib/trusty/uuid.h>
-#include <lib/vmm_obj_service/vmm_obj_service.h>
-#include <lk/init.h>
-#include <panic.h>
-#include <platform/interrupts.h>
-#include <spinlock.h>
-#include <stdio.h>
-#include <streams.h> /* stubs for stdin, stdout, stderr */
-#include "wrappers/include/reflist.h"
-
-#include "error.h"
-#include "config.h" /* for LK_LOGLEVEL_RUST */
+pub fn sync_cache_range<T>(slice: &[T]) {
+    let range = slice.as_ptr_range();
+    // SAFETY: The caller passes in a valid slice and the
+    // only side effects of the kernel function are on the cache.
+    unsafe {
+        crate::sys::arch_sync_cache_range(
+            range.start.addr(),
+            range.end.byte_offset_from_unsigned(range.start),
+        );
+    }
+}
